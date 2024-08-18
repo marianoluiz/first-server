adf2d3b6f81356042cdfc867bfac61cd6d63cbc5
Mariano Luiz King <kmarianu@gmail.com>
2024-08-18 21:12:05 +0800

    ignored npm modules

--- CD220Labs/http_server/index-with-require.js
+++ CD220Labs/http_server/index-with-require.js
@@ -9,6 +9,10 @@ const requestListener = function (req, res) {
     res.writeHead(200); // Set the status code to 200 (OK)
     // Send the response with the current date from the 'today' module
     res.end(`Hello, World! The date today is ${today.getDate()}`);
+
+    let dateVal = today.getDate();
+
+    console.log("\n\n" + dateVal);
 };
 
 // Define the port number
@@ -20,3 +24,17 @@ const server = http.createServer(requestListener);
 // Start the server and listen on the specified port
 server.listen(port);
 console.log('Server listening on port: ' + port);
+
+
+/* cat index.js 
+
+cat: Short for "concatenate", cat is a command-line utility used to read and display the contents of files.
+*/
+
+/* run: node index.js */
+
+/* ping the application: curl.exe localhost:8080 
+
+it means http request
+Checks if a web server is responding to HTTP requests.
+*/
--- CD220Labs/http_server/today.js
+++ CD220Labs/http_server/today.js
@@ -4,3 +4,17 @@ module.exports.getDate = function getDate() {
     let aestTime = new Date().toLocaleString("en-US", {timeZone: "Australia/Brisbane"});
     return aestTime; // Return the formatted date and time
 };
+
+/* 
+
+Export Function Use module.exports to export a function named getDate from the module.
+
+Date Formatting Inside the getDate function, get the current date and time using new Date().toLocaleString(“en-US”, {timeZone: “Australia/Brisbane”}). This formats the date and time according to the timezone “Australia/Brisbane”.
+
+Return Value Return the formatted date and time using return aestTime;.
+
+
+
+
+
+*/
